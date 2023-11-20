e84e4a4afbf1514923abe12c9d5f34831384bbce
Vadim-080 <vadim-080@yandex.ru>
2023-11-20 13:22:32 +0300

    PlanirovFinansov

--- app/src/main/java/com/example/planirfinansov6/Zastavka0.java
+++ app/src/main/java/com/example/planirfinansov6/Zastavka0.java
@@ -1,5 +1,6 @@
 package com.example.planirfinansov6;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,22 +13,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Zastavka0 extends AppCompatActivity {
     private TextView tv;
-    private ImageView iv;
+    private ImageView iv1, iv2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zastavka);
+
         tv = (TextView) findViewById(R.id.tv);
-        iv = (ImageView) findViewById(R.id.iv);
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.zastav_anim1);
-        tv.startAnimation(myanim);
-        iv.startAnimation(myanim);
+        iv1 = (ImageView) findViewById(R.id.iv1);
+        iv2 = (ImageView) findViewById(R.id.iv2);
+        Animation myanim1 = AnimationUtils.loadAnimation(this, R.anim.zastav_anim1);
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.zastav_anim2);
+        iv1.startAnimation(myanim1);
+        iv2.startAnimation(myanim2);
         final Intent i = new Intent(this, MainActivity.class);
         Thread timer = new Thread() {
             public void run() {
                 try {
-                    sleep(17000);
+                    sleep(15000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
